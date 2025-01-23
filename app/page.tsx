0563f2ca72207e54c1f7d4ee4333201138eb1d6f
Nevzat Atalay <nevzatatalay79@gmail.com>
2025-01-23 20:52:49 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import {Button} from '@/components/ui/button'
+import { Progress } from "@/components/ui/progress"
+import { LoginForm } from "@/components/login-form"
+
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function Home() {
+  return (
+    <div>
+      <Progress value={75}/>
+      <LoginForm />
+      <Button>
+        Test
+      </Button>
+      <DropdownMenu>
+  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+    </div>
+  );
+}
