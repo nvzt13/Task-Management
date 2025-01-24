fcdaf782aa0c1ca97a53f926883ce2c840d6c662
Nevzat Atalay <nevzatatalay79gmail.com>
2025-01-24 13:29:37 +0300

    kurulum ortamı hazırlandı

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,14 @@
-import Image from "next/image";
-import {Button} from '@/components/ui/button'
-import { Progress } from "@/components/ui/progress"
-import { LoginForm } from "@/components/login-form"
+import {prisma} from '@/lib/prisma'
+import Link from 'next/link';
 
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export default function Home() {
+ 
   return (
     <div>
-      <Progress value={75}/>
-      <LoginForm />
-      <Button>
-        Test
-      </Button>
-      <DropdownMenu>
-  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
-  </DropdownMenuContent>
-</DropdownMenu>
-
+      <Link href="/api/auth/signin">
+      test
+      </Link>
     </div>
   );
 }
