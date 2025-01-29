c22147caee47eae0b4d543333c571b30c8b3dc19
Nevzat Atalay <nevzatatalay79gmail.com>
2025-01-29 12:38:59 +0300

    projenin genel yapısı değiştirildi

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
-import {prisma} from '@/lib/prisma'
-import Link from 'next/link';
+import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import Image from "next/image";
+import Link from "next/link";
 
-
-export default function Home() {
- 
-  return (
-    <div>
-      <Link href="/api/auth/signin">
-      test
-      </Link>
-    </div>
-  );
+export default async function Home() {
+  const session = await auth();
+  if (session?.user) {
+    return (
+      <>
+        giriş yok
+        <Link href={"/api/auth/signin"}>sign in</Link>
+      </>
+    );
+  }
+  return <div>
+    {session?.user?.name}
+  </div>;
 }
